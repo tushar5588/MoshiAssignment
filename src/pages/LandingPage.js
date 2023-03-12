8d6b29ead11b0be33210bf37e73c0a2490a29ec7
Tushar Garg <tusharmunnu@gmail.com>
2023-03-12 16:30:34 +0530

    first commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,284 @@
+import "../styles/LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page-front-developer">
+      <img className="landing-page-child" alt="" src="/rectangle-93.svg" />
+      <img className="landing-page-item" alt="" src="/vector-24.svg" />
+      <img className="landing-page-inner" alt="" src="/ellipse-10.svg" />
+      <img className="group-icon" alt="" src="/group-21.svg" />
+      <a href="#" className="contact-now">
+        Contact Now
+      </a>
+      <img className="group-icon1" alt="" src="/group.svg" />
+      <a href="#" className="skyclub">
+        Skyclub
+      </a>
+      <div className="rectangle-div" />
+      <img className="ellipse-icon" alt="" src="/ellipse-11.svg" />
+      <a href="#" className="sky-wallet">
+        Sky Wallet
+      </a>
+
+      <svg
+        className="akar-iconsdiscord-fill pointer"
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+      >
+        <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z" />
+      </svg>
+      <svg
+        className="akar-iconstwitter-fill pointer"
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+      >
+        <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+      </svg>
+      <img className="landing-page-child1" alt="" src="/group-110.svg" />
+      <b className="the-web3-platform-container">
+        <p className="the-web3-platform">The Web3 Platform Built for You</p>
+      </b>
+      <div className="istockphoto-1273852653-170667a-parent">
+        <img
+          className="istockphoto-1273852653-170667a-icon"
+          alt=""
+          src="/istockphoto1273852653170667a-1@2x.png"
+        />
+        <img
+          className="istockphoto-1273852653-170667a-icon1"
+          alt=""
+          src="/istockphoto1273852653170667a-3@2x.png"
+        />
+        <img
+          className="istockphoto-1273852653-170667a-icon2"
+          alt=""
+          src="/istockphoto1273852653170667a-2@2x.png"
+        />
+        <img
+          className="istockphoto-1273852653-170667a-icon3"
+          alt=""
+          src="/istockphoto1273852653170667a-4@2x.png"
+        />
+      </div>
+      <div className="landing-page-child2" />
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <div className="group-item" />
+        <div className="drag">Drag</div>
+      </div>
+      <img className="landing-page-child3" alt="" src="/group-112.svg" />
+      <img className="landing-page-child4" alt="" src="/group-117.svg" />
+      <div className="landing-page-child5" />
+      <img className="mask-group-2-icon" alt="" src="/mask-group-2@2x.png" />
+      <img className="mask-group-3-icon" alt="" src="/mask-group-3@2x.png" />
+      <b className="tanukibles">Tanukibles</b>
+      <b className="tanuki-nfts-up-container">
+        <p className="the-web3-platform">9999 Tanuki NFTs up for grabs</p>
+      </b>
+      <b className="play-2-earn"> Play 2 Earn Game</b>
+      <img className="landing-page-child6" alt="" src="/group-122.svg" />
+      <img className="vector-icon" alt="" src="/vector-48.svg" />
+      <img className="landing-page-child7" alt="" src="/vector-46.svg" />
+      <img className="vector-172-icon" alt="" src="/vector-172.svg" />
+      <img className="landing-page-child8" alt="" src="/vector-45.svg" />
+      <div className="mask-group-5-parent">
+        <img className="mask-group-5-icon" alt="" src="/mask-group-5.svg" />
+        <b className="your-everyday-crypto">Your everyday crypto wallet</b>
+        <div className="group-inner" />
+        <div className="rectangle-70-2" />
+        <div className="rectangle-71-2" />
+        <div className="rectangle-72-2" />
+        <div className="use-across-any">{`Use across any app and currency `}</div>
+        <div className="one-place-for">
+          One place for all your Web3 possessions
+        </div>
+        <div className="simple-and-secure">Simple and secure</div>
+        <div className="works-everywhere">Works everywhere</div>
+        <img
+          className="fluentcurrency-dollar-euro-24-icon"
+          alt=""
+          src="/fluentcurrencydollareuro24filled.svg"
+        />
+        <img className="epplace-icon" alt="" src="/epplace.svg" />
+        <img
+          className="eos-iconssecure-data-outlined"
+          alt=""
+          src="/eosiconssecuredataoutlined.svg"
+        />
+        <img
+          className="icbaseline-work-outline-icon"
+          alt=""
+          src="/icbaselineworkoutline.svg"
+        />
+
+        <div className="group-child2" />
+        <img className="group-child1" alt="" src="/group-106.svg" />
+        <b className="exclusive-for-you">
+          Exclusive, for you and your community
+        </b>
+        <div className="rectangle-73-2" />
+        <div className="rectangle-74-2" />
+        <div className="rectangle-75-2" />
+        <div className="group-child3" />
+        <div className="chat-play-games">Chat, play games, and host events</div>
+        <div className="create-your-own">
+          Create your own DAO and Tokens in 1 click
+        </div>
+        <div className="create-eco-friendly-nft">{`Create eco-friendly NFT artwork and tickets `}</div>
+        <div className="offer-exclusive-content">
+          Offer exclusive content and NFT memberships
+        </div>
+        <img className="vector-198-icon" alt="" src="/vector-198.svg" />
+        <img className="vector-199-icon" alt="" src="/vector-199.svg" />
+        <img className="group-child4" alt="" src="/group-107.svg" />
+        <img
+          className="icon-park-outlineticket-one"
+          alt=""
+          src="/iconparkoutlineticketone.svg"
+        />
+        <img className="bxsoffer-icon" alt="" src="/bxsoffer.svg" />
+      </div>
+      <div className="rectangle-group">
+        <button className="group-child5 pointer">Learn more</button>
+      </div>
+      <div className="landing-page-child9" />
+      <b className="communities-are-the-container">
+        <p className="the-web3-platform">
+          Communities are the lifeblood of society.
+        </p>
+      </b>
+      <div className="decentralised-communities-are-container">
+        <p className="the-web3-platform">
+          Decentralised communities are going to be the lifeblood of the
+          metaverse.
+        </p>
+        <p className="the-web3-platform"></p>
+        <p className="the-web3-platform">&nbsp;</p>
+      </div>
+      <div className="metasky-aims-to">
+        Metasky aims to empower influencers, creators and brands to build happy,
+        motivated Web3 communities with a strong sense of belongingness.
+      </div>
+      <img className="landing-page-child10" alt="" src="/group-126.svg" />
+      <b className="organization-structure">Organization Structure</b>
+      <b className="community">COMMUNITY</b>
+      <b className="investors">INVESTORS</b>
+      <b className="core-team">{`CORE TEAM `}</b>
+      <div className="landing-page-child11" />
+      <div className="landing-page-child12" />
+      <div className="landing-page-child13" />
+      <div className="landing-page-child14" />
+      <img className="union-2-icon" alt="" src="/union-2.svg" />
+      <img className="union-3-icon" alt="" src="/union-3.svg" />
+      <b className="b">60%</b>
+      <b className="b1">20%</b>
+      <b className="b2">20%</b>
+      <img className="group-85-icon" alt="" src="/group-85.svg" />
+      <img className="group-86-icon" alt="" src="/group-86.svg" />
+      <div className="web3">WEB3</div>
+      <div className="landing-page-child15" />
+      <div className="group-parent">
+        <div className="rectangle-container">
+          <div className="group-child6" />
+          <div className="group-child6" />
+          <div className="group-child8" />
+          <div className="group-child9" />
+          <div className="group-child10" />
+          <div className="group-child11" />
+          <div className="group-child12" />
+          <div className="group-child13" />
+          <div className="group-child14" />
+        </div>
+        <div className="group-div">
+          <div className="group-child15" />
+          <div className="group-child15" />
+          <div className="group-child17" />
+          <div className="group-child18" />
+          <div className="group-child19" />
+          <div className="group-child20" />
+          <div className="group-child21" />
+          <div className="group-child22" />
+          <div className="group-child23" />
+        </div>
+        <div className="rectangle-111-2-parent">
+          <div className="rectangle-111-2" />
+          <div className="rectangle-111-2" />
+          <div className="rectangle-113-2" />
+          <div className="rectangle-114-2" />
+          <div className="rectangle-115-2" />
+          <div className="rectangle-116-2" />
+          <div className="rectangle-117-2" />
+          <div className="rectangle-118-2" />
+          <div className="rectangle-119-2" />
+        </div>
+      </div>
+      <div className="landing-page-child16" />
+      <img className="landing-page-child17" alt="" src="/vector-53.svg" />
+      <img className="landing-page-child18" alt="" src="/vector-54.svg" />
+      <div className="rectangle-parent1">
+        <div className="group-child24" />
+        <div className="group-child25" />
+        <b className="ankit-arora">
+          <p className="the-web3-platform">Ankit Arora</p>
+        </b>
+        <div className="co-founder">Co-Founder</div>
+        <img className="group-child26" alt="" src="/group-134@2x.png" />
+      </div>
+      <div className="rectangle-120-2-parent">
+        <div className="group-child24" />
+        <div className="group-child25" />
+        <img className="mask-group-7-icon" alt="" src="/mask-group-7.svg" />
+        <b className="ankit-arora-2">
+          <p className="the-web3-platform">Ankit Arora</p>
+        </b>
+        <div className="co-founder-2">Co-Founder</div>
+        <img className="image-17-2-icon" alt="" src="/image-17-2@2x.png" />
+      </div>
+      <div className="rectangle-120-3-parent">
+        <div className="group-child24" />
+        <div className="group-child25" />
+        <img className="mask-group-7-icon" alt="" src="/mask-group-8.svg" />
+        <b className="ankit-arora-3">
+          <p className="the-web3-platform">Ankit Arora</p>
+        </b>
+        <div className="co-founder-3">Co-Founder</div>
+        <img className="image-17-3-icon" alt="" src="/image-17-2@2x.png" />
+      </div>
+      <b className="the-metasky-team">The metasky Team</b>
+      <img className="landing-page-child20" alt="" src="/group-116@2x.png" />
+      <div className="landing-page-child21" />
+      <div className="group-container">
+        <div className="coming-soon-parent">
+          <div className="coming-soon">Coming Soon</div>
+          <div className="coming-soon-2">Coming Soon</div>
+          <div className="coming-soon-3">Coming Soon</div>
+        </div>
+        <div className="coming-soon-4-parent">
+          <div className="coming-soon">Coming Soon</div>
+          <div className="coming-soon-2">Coming Soon</div>
+          <div className="coming-soon-6">Coming Soon</div>
+          <div className="coming-soon-7">Coming Soon</div>
+        </div>
+      </div>
+      <div className="landing-page-child22" />
+      <div className="landing-page-child23" />
+      <img className="landing-page-child24" alt="" src="/group-118.svg" />
+      <img className="group-88-icon" alt="" src="/group-88.svg" />
+      <button className="landing-page-child25 pointer">JOIN THE TEAM</button>
+      <button className="landing-page-child26 pointer">BUSINESS INQUIRIES</button>
+      <img className="group-89-icon" alt="" src="/group-89.svg" />
+      <img className="group-98-icon" alt="" src="/group-98.svg" />
+      <img className="group-107-icon" alt="" src="/group-1071.svg" />
+      <img className="group-116-icon" alt="" src="/group-1161.svg" />
+      <img className="landing-page-child27" alt="" src="/group-114.svg" />
+    </div>
+  );
+};
+
+export default LandingPage;
